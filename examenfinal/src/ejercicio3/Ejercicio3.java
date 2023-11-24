fa79624b04f37cc6296cb265910ef18267ea9170
LEGCIUS <143227122+LEGCIUS@users.noreply.github.com>
2023-11-24 15:04:41 -0600

    Examen

---
+++ examenfinal/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+
+/**
+ *
+ * @author pablonoguera
+ */
+public class Ejercicio3 {
+
+    private static BufferedReader BufferedReader;
+       public static void calcularValorInventario() throws FileNotFoundException {
+          System.out.println("");
+          System.out.println("Ejercicio 3");
+          System.out.println("");
+          System.out.println("Escribe un programa en Java que lea un archivo "
+                  + "de texto llamado \"datos.txt\" \nque contiene información "
+                  + "sobre productos (nombre, precio y cantidad en stock). "
+                  + "\nCalcula el valor total del inventario y guarda el "
+                  + "resultado en un nuevo archivo llamado \"inventario.txt\".\n");
+        // Implementa la lógica del ejercicio 3 aquí
+          
+      
+       
+       }
+              
+}
+
+ 
+
